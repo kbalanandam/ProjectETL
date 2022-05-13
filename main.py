e640d55c64285c3372a3ec9090c58b5807f560e8
kbalanandam <40485038+kbalanandam@users.noreply.github.com>
2022-05-13 23:39:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import preValidation
+import loadCsv
+from datetime import datetime
+import logging
+import os
+import Config
+from Config import fileConfig
+
+
+def main():
+    for x in os.listdir(fileConfig['filepath']):
+        if x.endswith(fileConfig['extension']):
+            localtime = datetime.now()
+            logfile = 'csvProcess_'+localtime.strftime('%Y%m%d%H%M%S')+'.log'
+            logging.basicConfig(filename=logfile, level=logging.INFO,
+                                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            logging.info('csv file found: ' + x)
+            Config.FileCount = Config.FileCount + 1
+
+            if preValidation.validation(x):
+                logging.info('file: ' + x + ' validation completed successfully.')
+            else:
+                logging.info('file: ' + x + ' validation failed.')
+            if loadCsv.fileload(x):
+                logging.info('file: ' + x + ' process completed successfully.')
+            else:
+                logging.info('file: ' + x + ' process failed.')
+
+
+if __name__ == '__main__':
+    main()
+
