f5be059679c3ef2a6bad9e3c0858484c01967c15
kbalanandam@gmail.com <nkoyyalamudi@tollplus.com>
2022-05-13 23:48:14 +0530

    internal project folder name changed

---
+++ csvtodb/loadCsv.py
@@ -0,0 +1,39 @@
+import logging
+import csv
+import mysql.connector
+from mysql.connector import Error
+from Config import dbConfig
+from Config import fileConfig
+name = __name__
+
+
+def fileload(filename):
+    logging.info('Processing file -> ' + filename)
+    try:
+
+        connection = mysql.connector.connect(host=dbConfig['host'],
+                                             database=dbConfig['database'],
+                                             user=dbConfig['user'],
+                                             password=dbConfig['password'])
+
+        if connection.is_connected():
+            dbinfo = connection.get_server_info()
+            logging.info('Connected to MySQL Server database version ' + dbinfo)
+            cursor = connection.cursor()
+
+            with open(fileConfig['validpath'] + filename, "r") as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=fileConfig['delimiter'])
+                for row in csv_reader:
+                    cursor.execute(
+                        "INSERT INTO VetaranPlates(PlateState, LicensePlate, StartEffectiveDate, EndEffectiveDate, CreatedDate, CreatedUser, UpdatedDate, UpdatedUser) VALUES(%s, %s, %s, %s, now(),'loadCsv.load',now(),'loadCsv.load')",
+                        row)
+                connection.commit()
+        return True
+    except Error as e:
+        logging.error('Program: '+name+',   Error while connecting to the database: ' + str(e))
+        return False
+
+
+
+
+
