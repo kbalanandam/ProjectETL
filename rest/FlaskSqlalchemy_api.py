25ede79865eaba05caf1df90872160491da1b4d8
kbalanandam@gmail.com <nkoyyalamudi@tollplus.com>
2022-06-09 12:56:54 +0530

    try block added to api code.

--- rest/FlaskSqlalchemy_api.py
+++ rest/FlaskSqlalchemy_api.py
@@ -7,23 +7,29 @@ from functools import wraps
 
 @app.route('/api/users', methods=['GET'])
 def api_get_users():
-    all_users = []
-    users = User.query.all()
-    for i in users:
-        user = {'name': i.username, 'email': i.email, 'id': i.id}
-        all_users.append(user)
+    try:
+        all_users = []
+        users = User.query.all()
+        for i in users:
+            user = {'name': i.username, 'email': i.email, 'id': i.id}
+            all_users.append(user)
 
-    return jsonify({'users': all_users})
+        return jsonify({'users': all_users})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 
 @app.route('/api/users/add', methods=['POST'])
 def api_add_users():
-    user = request.get_json()
-    adduser = User(username=user['name'], email=user['email'])
-    db.session.add(adduser)
-    db.session.commit()
+    try:
+        user = request.get_json()
+        adduser = User(username=user['name'], email=user['email'])
+        db.session.add(adduser)
+        db.session.commit()
 
-    return jsonify('user added.')
+        return jsonify('user added.')
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 
 if __name__ == '__main__':
