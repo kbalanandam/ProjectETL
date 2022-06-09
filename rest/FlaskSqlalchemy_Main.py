74893fb9dd8555b143fdea913e729dc6c11c2c9a
kbalanandam <kbalanandam@gmail.com>
2022-06-09 23:44:56 +0530

    UnknownException class added

--- rest/FlaskSqlalchemy_Main.py
+++ rest/FlaskSqlalchemy_Main.py
@@ -18,25 +18,13 @@ py = Category(name='Python')
 db.session.add(py)
 db.session.commit()
 
-# Post(title='Oracle!', body='Oracle is the best RDBMS', category=py, user=Bala)
-
-
-userid = db.session.query(User).with_entities(User.id).filter(User.username == 'Bala').one()
-print(userid[0])
-categoryid = db.session.query(Category).with_entities(Category.id).filter(Category.name == 'Python').one()
-print(categoryid[0])
-add_post = Post(title='Hello Python, how are you ?', body='python is the best', category_id=categoryid[0],
-                user_id=userid[0])
-db.session.add(add_post)
-db.session.commit()
-
 all_users = []
 users = User.query.all()
 
 Json_result = {
     "user": "Bala",
     "Category": [{
-        "category": "Python",
+        "name": "Python",
         "posts": [{
             "title": "api",
             "body": "rest apis are good."
