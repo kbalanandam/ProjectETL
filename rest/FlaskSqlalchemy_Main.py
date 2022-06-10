940e8c684f516e9c49ed366bb2e27fc0a7255f48
kbalanandam <kbalanandam@gmail.com>
2022-06-10 08:39:50 +0530

    UnknownException class added

--- rest/FlaskSqlalchemy_Main.py
+++ rest/FlaskSqlalchemy_Main.py
@@ -1,5 +1,5 @@
 from FlaskSqlalchemy_app import db, User, Post, Category
-
+import json
 db.drop_all()
 db.create_all()
 
@@ -13,9 +13,17 @@ db.session.add(Bala)
 db.session.commit()
 
 py = Category(name='Python')
-# p = Post(title='Snakes', body='Ssssssss')
+p = Post(title='Hi Python!', body='Python is good', category_id=1, user_id=1)
 # py.posts.append(p)
 db.session.add(py)
+db.session.add(p)
+db.session.commit()
+
+py = Category(name='Oracle')
+p = Post(title='Hi Oracle!', body='Oracle is good', category_id=2, user_id=1)
+# py.posts.append(p)
+db.session.add(py)
+db.session.add(p)
 db.session.commit()
 
 all_users = []
@@ -44,3 +52,22 @@ Json_result = {
     }]
 
 }
+
+json_result =[]
+for u in db.session.query(User).all():
+    user = {"user": u.username}
+    posts = []
+    category = []
+    for p in db.session.query(Post).filter(Post.user_id == u.id).all():
+        post = {"title": p.title, "body": p.body}
+        for c in db.session.query(Category).filter(Category.id == p.category_id).all():
+            cs = {"name":c.name}
+            category.append(cs)
+            print("User: {}, Category: {}, title: {}, Post: {} ".format(u.username, c.name, p.title, p.body))
+            json_result.append(category)
+        print("User: {}, Category: {}".format(u.username, c.name))
+        json_result.append(posts)
+    print("User: {}".format(u.username))
+    json_result.append(user)
+print(json.dumps(json_result))
+
