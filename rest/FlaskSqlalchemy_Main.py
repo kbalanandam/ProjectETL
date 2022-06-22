01ff03a78596bb29be387197e7a5654d48cca612
kbalanandam <kbalanandam@gmail.com>
2022-06-22 20:29:26 +0530

    *_Main.py updated

--- rest/FlaskSqlalchemy_Main.py
+++ rest/FlaskSqlalchemy_Main.py
@@ -6,7 +6,7 @@ db.create_all()
 
 admin = User(username='admin', email='admin@example.com')
 guest = User(username='guest', email='guest@example.com')
-Bala = User(username='Bala', email='bala@example.com')
+Bala = User(username='bala', email='bala@example.com')
 
 db.session.add(admin)
 db.session.add(guest)
@@ -35,57 +35,3 @@ db.session.commit()
 
 all_users = []
 users = User.query.all()
-
-Json_result = {
-    "user": "Bala",
-    "Category": [{
-        "name": "Python",
-        "posts": [{
-            "title": "api",
-            "body": "rest apis are good."
-        }, {
-            "title": "Python",
-            "body": "Python apis are good."
-        }]
-    }, {
-        "name": "RDBMS",
-        "posts": [{
-            "title": "Oracle",
-            "body": "Oracle is good."
-        }, {
-            "title": "MySQL",
-            "body": "MySQL is good."
-        }]
-    }]
-
-}
-
-posts = []
-category = []
-json_result = []
-name = 'admin'
-post = {}
-cs = {}
-user = db.session.query(User).filter(User.username == name).one()
-for a in db.session.query(Post).filter(Post.user_id == user.id).all():
-    for c in db.session.query(Category).filter(Category.id == a.category_id).all():
-
-        for r in db.session.query(Post).filter(Post.category_id == c.id, Post.user_id == user.id).all():
-            print(user.id)
-            print(c.id)
-            print(r.id)
-            post['title']=r.title
-            post['body']= r.body
-            posts.append(post)
-            print(posts)
-
-        cs['name'] = c.name
-        cs['posts']= posts
-        category.append(cs)
-        print(cs)
-    json_result.append({"user": user.username, "category": category})
-#print(json_result)
-
-
-
-print(json.dumps(json_result,indent=4))
