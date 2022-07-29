64c1485f42aab2332f42e5bd5afb531c3f165009
kbalanandam@gmail.com <nkoyyalamudi@tollplus.com>
2022-07-29 15:29:14 +0530

    mvc type conversion in progress

--- rest/app.py
+++ rest/app.py
@@ -2,12 +2,14 @@ from model import db, User, Post, Category, app
 from flask import request, jsonify
 import json
 from flask_cors import CORS
+from flask_restful import reqparse, Api, Resource
 
 
 # import jwt
 # from werkzeug.security import generate_password_hash, check_password_hash
 # from functools import wraps
 CORS(app)
+api = Api(app)
 
 
 class UnknownException(Exception):
@@ -28,174 +30,162 @@ class AlreadyExists(Exception):
         return self.__value
 
 
-class Posts:
-    def __int__(self):
-        pass
+class UsersApi(Resource):
 
-    @staticmethod
-    def get_posts(uid, cid):
-        posts = []
-        for p in db.session.query(Post).filter(Post.user_id == uid, Post.category_id == cid).all():
-            posts.append({"title": p.title, "body": p.body})
-        return posts
-
-    @staticmethod
-    def add_posts(**post):
-        try:
-
-            if db.session.query(User).with_entities(User.id).filter(User.login == post['user']).count() == 0:
-                raise UnknownException('unknown user.')
-            elif db.session.query(Category).with_entities(Category.id).filter(
-                    Category.name == post['category']).count() == 0:
-                raise UnknownException('unknown Category.')
-
-            userid = db.session.query(User).with_entities(User.id).filter(User.login == post['user']).one()
-            category_id = db.session.query(Category).with_entities(Category.id).filter(
-                Category.name == post['category']).one()
-            add_post = Post(title=post['title'], body=post['body'], category_id=category_id.id, user_id=userid.id)
-            db.session.add(add_post)
-            db.session.commit()
-            message = 'post created.'
-
-            return jsonify(message)
-        except Exception as e:
-            return jsonify({'message': 'Error: '+str(e)})
-
-
-class Categories:
-    def __int__(self):
-        pass
-
-    @staticmethod
-    def add_category(cname):
-        try:
-            category = Category(name=cname)
-            db.session.add(category)
-            db.session.commit()
-            return jsonify('category added.')
-        except Exception as e:
-            return jsonify({'message': 'Error: '+str(e)})
-
-    @staticmethod
-    def get_category(cid):
-        c = db.session.query(Category).filter(Category.id == cid).one()
-        return c.name
-
-
-class Users:
-    def __int__(self):
-        pass
-
-    @staticmethod
-    def add_users(**user):
-        try:
-            if db.session.query(User).filter(User.login == user['login']).count() > 0:
-                raise AlreadyExists("user already exists.")
-            adduser = User(login=user['login'], email=user['email'], firstname=user['firstname'], lastname=user['lastname'], gender=user['gender'])
-            db.session.add(adduser)
-            db.session.commit()
-            return jsonify({"message": "user {}, is created successfully.".format(user['login'])})
-        except Exception as e:
-            return jsonify({'message': 'Error: '+str(e)})
-
-    @staticmethod
-    def get_users():
+    def get(self):
         try:
             all_users = []
             users = User.query.all()
             for i in users:
-                user = {'firstname': i.firstname, 'lastname': i.lastname, 'gender': i.gender, 'login': i.login, 'email': i.email, 'id': i.id}
+                user = {'firstname': i.firstname, 'lastname': i.lastname, 'gender': i.gender, 'user': i.user,
+                        'email': i.email, 'id': i.id, 'createdon': i.createddate}
                 all_users.append(user)
 
             return jsonify({'users': all_users})
         except Exception as e:
-            return jsonify({'message': 'Error: '+str(e)})
+            return jsonify({'message': 'Error: ' + str(e)})
 
-    @staticmethod
-    def get_posts(user):
+    def post(self):
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('user', type=str, required=True, help='user name cannot be blank')
+        parser.add_argument('password', type=str, required=True, help='this cannot be blank')
+        parser.add_argument('email', type=str, required=True, help='email cannot be blank')
+        parser.add_argument('firstname', type=str, required=True)
+        parser.add_argument('lastname', type=str, required=True)
+        parser.add_argument('gender', type=str, required=True)
         try:
-            user_posts = []
+            _user = parser.parse_args()
+            if User.find_by_username(_user['user']):
+                return {'messageType': 'Error', 'message': "An user with name '{}' already exists.".format(_user['user'])}, 400
+            new = User(user=_user['user'], password=_user['password'], email=_user['email'], firstname=_user['firstname'], lastname=_user['lastname'],
+                    gender=_user['gender'], createdby='UsersApi')
+            new.save_to_db()
+            return {'messageType': 'Success', "message": "user {}, is created successfully.".format(_user['user'])}
+        except Exception as e:
+            return {'messageType': 'Error', 'message': str(e)}
+
+
+class UserApi(Resource):
+
+    def get(self, user):
+        try:
+            user = User.find_by_username(user)
+            if user:
+                return User.json()
+            return {'messageType': 'Error', 'message': 'user not found'}, 404
+        except Exception as e:
+            return {'messageType': 'Error', 'message': str(e)}, 201
+
+    def delete(self, user):
+        try:
+            _user = User.find_by_username(user)
+            if _user:
+                User.delete_from_db(_user)
+                return {'messageType': 'Success', "message": "user deleted."}
+            return {'messageType': 'Error', 'message': 'user not found'}, 404
+
+        except Exception as e:
+            return {'messageType': 'Error', 'message': str(e)}
+
+
+class CategoriesApi(Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True, help='Category name cannot be blank')
+        parser.add_argument('description', type=str, required=False, help='Description is optional')
+        try:
+
+            _category = parser.parse_args()
+            if Category.find_by_name(_category['name']):
+                return {'messageType': 'Error', 'message': "A category with name '{}' already exists.".format(_category['name'])}, 400
+
+            new = Category(name=_category['name'], description=_category['description'], createdby='CategoriesApi')
+            new.save_to_db()
+            return {'messageType': 'Success', 'message': "Category {}, is created successfully.".format(_category['name'])}, 201
+        except Exception as e:
+            return {'messageType': 'Error', 'message': 'Error: ' + str(e)}
+
+
+class CategoryApi(Resource):
+
+    def get(self, category):
+        try:
+            _category = Category.find_by_name(category)
+            if _category:
+                return _category.json()
+            return {'messageType': 'Error', 'message': 'category not found'}, 404
+
+        except Exception as e:
+            return {'messageType': 'Error', 'message': str(e)}, 201
+
+    def delete(self, category):
+        try:
+            _category = Category.find_by_name(category)
+            if _category:
+                Category.delete_from_db(_category)
+                return {'messageType': 'Success', "message": "category deleted."}
+            return {'messageType': 'Error', 'message': 'category not found'}, 404
+
+        except Exception as e:
+            return {'messageType': 'Error', 'message': str(e)}
+
+
+class UserPostApi(Resource):
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('title', type=str, required=True, help='title cannot be blank')
+        parser.add_argument('body', type=str, required=True, help='body cannot be blank')
+        parser.add_argument('category', type=str, required=True, help='category cannot be blank')
+        parser.add_argument('user', type=str, required=True, help='user cannot be blank')
+        try:
+            _post = parser.parse_args()
+
+            _category = Category.find_by_name(_post['category'])
+            if _category is None:
+                return {'messageType': 'Error', 'message': 'category not found'}, 404
+
+            _user = User.find_by_username(_post['user'])
+            if _user is None:
+                return {'messageType': 'Error', 'message': 'user not found'}, 404
+
+            new = Post(title=_post['title'], body=_post['body'], cid=_category.id, uid=_user.id, createdby='UserPostApi')
+            new.save_to_db()
+
+            return {'messageType': 'Success', 'message': 'post created.'}, 201
+        except Exception as e:
+            return {'messageType': 'Error', 'message': str(e)}
+
+
+class PostbyUserApi(Resource):
+
+    def get(self, user):
+        try:
+
             user_category = []
             category = {}
 
-            user = db.session.query(User).filter(User.login == user).one()
-            for c in db.session.query(Post).with_entities(Post.category_id).filter(Post.user_id == user.id).distinct():
+            _user = User.find_by_username(user)
+            if _user is None:
+                return {'messageType': 'Error', 'message': 'user not found'}, 404
+            for c in db.session.query(Post).with_entities(Post.category_id).filter(Post.user_id == _user.id).distinct():
                 for a in db.session.query(Category).filter(Category.id == c.category_id):
-                    category['name'] = Categories.get_category(a.id)
-                    category['posts'] = Posts.get_posts(user.id, a.id)
+                    category['name'] = a.name
+                    category['posts'] = Post.find_post(_user.id, a.id)
                 user_category.append({"name": category['name'], "posts": category['posts']})
-            user_posts.append({"user": user.login, "category": user_category})
-            return user_posts
+            return {'user': user, 'category': user_category}
         except Exception as e:
-            return jsonify({'message': 'Error: '+str(e)})
-
-    @staticmethod
-    def del_user(user):
-        try:
-            if db.session.query(User).filter(User.login == user).count() == 0:
-                raise UnknownException("unknown user.")
-
-            userid = db.session.query(User).with_entities(User.id).filter(User.login == user).one()
-            userid = User.query.get(userid)
-            db.session.delete(userid)
-            db.session.commit()
-            return jsonify({"message": "user deleted."})
-        except Exception as e:
-            return jsonify({'message': str(e)})
+            return jsonify({'messageType': 'Error', 'message': str(e)})
 
 
-@app.route('/api/users', methods=['GET'])
-def api_get_users():
-    return Users.get_users()
-
-
-@app.route('/api/users/add', methods=['POST'])
-def api_add_users():
-    try:
-        user = request.get_json()
-        return Users.add_users(**user)
-    except Exception as e:
-        return jsonify({'message': 'Error: '+str(e)})
-
-
-@app.route('/api/category/add', methods=['POST'])
-def api_add_category():
-    try:
-        category = request.get_json()
-        if category['name']:
-            return Categories.add_category(category['name'])
-    except Exception as e:
-        return jsonify({'message': 'Error: '+str(e)})
-
-
-@app.route('/api/posts/add', methods=['POST'])
-def api_add_posts():
-    try:
-        post = request.get_json()
-        return Posts.add_posts(**post)
-    except Exception as e:
-        return jsonify({'message': 'Error: '+str(e)})
-
-
-@app.route('/api/posts/<login>', methods=['GET'])
-def api_get_posts(login):
-    try:
-
-        if db.session.query(User).filter(User.login == login).count() == 0:
-            raise UnknownException('unknown user.')
-        return json.dumps(Users.get_posts(login), indent=4)
-
-    except Exception as e:
-        return jsonify({'message': 'Error: '+str(e)})
-
-
-@app.route('/api/delete/user', methods=['POST'])
-def api_del_users():
-    try:
-        user = request.get_json()
-        return Users.del_user(user['name'])
-    except Exception as e:
-        return jsonify({'message': 'Error: '+str(e)})
+api.add_resource(UsersApi, '/api/users', endpoint='users')
+api.add_resource(UserApi, '/api/users/<user>', endpoint='user')
+api.add_resource(CategoriesApi, '/api/categories', endpoint='categories')
+api.add_resource(CategoryApi, '/api/categories/<category>', endpoint='category')
+api.add_resource(UserPostApi, '/api/posts', endpoint='creatpost')
+api.add_resource(PostbyUserApi, '/api/posts/<user>', endpoint='userposts')
 
 
 if __name__ == '__main__':
