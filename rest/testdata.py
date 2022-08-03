33e4a183036c18314f407c5036164e45f0a318a7
kbalanandam@gmail.com <nkoyyalamudi@tollplus.com>
2022-08-03 17:22:20 +0530

    testdata.py updated

--- rest/testdata.py
+++ rest/testdata.py
@@ -1,9 +1,9 @@
-# only run this, if needed some test data set up. otherwise not needed.
-# Below lines to create test data using curl
 # curl -v http://127.0.0.1:5000/api/users
-# curl -d "{\"email\":\"sampath@gmail.com\",\"name\":\"Sampath\"}" -H "Content-Type: application/json" http://127.0.0.1:5000/api/users/add
-# curl -d "{\"title\":\"test oracle post\",\"user\":\"Bala\",\"category\":\"Oracle\",\"body\":\"Oracle learning is always fun!\"}" -H "Content-Type: application/json" http://127.0.0.1:5000/api/posts/add
-
+#curl -d "{\"email\":\"sampath@gmail.com\",\"login\":\"Sampath\",\"firstname\":\"Sampath\",\"lastname\":\"K\",\"gender\":\"M\"}" -H "Content-Type: application/json" http://127.0.0.1:5000/api/users
+#curl -d "{\"title\":\"test oracle post\",\"user\":\"bala\",\"category\":\".NET\",\"body\":\".NET learning is always fun!\"}" -H "Content-Type: application/json" http://127.0.0.1:5000/api/posts
+# #curl -d "{\"name\":\"anil\"}" -H "Content-Type: application/json" http://127.0.0.1:5000/api/delete/user
+# curl -d "{\"name\":\"Oracle\"}" -H "Content-Type: application/json" http://127.0.0.1:5000/api/category/add
+#curl  http://127.0.0.1:5000/api/posts/bala ## to get user posts
 from model import db, User, Post, Category
 
 
